788efa7332cd72c8ec4d26165affb2bfbae30de7
mixelpixel <mixelpix@mac.com>
2017-08-23 05:19:15 -0400

    linter, odds and ends fixes

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -6,130 +6,130 @@ const Comment = mongoose.model('Comment');
 const STATUS_USER_ERROR = 422;
 
 const createPost = (req, res) => {
-    const { title, text } = req.query;
-    const newPost = new Post({ title, text });
-    newPost.save()
+  const { title, text } = req.query;
+  const newPost = new Post({ title, text });
+  newPost.save()
         .then((newPost) => {
-            res.json(newPost);
+          res.json(newPost);
         })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 };
 
 const listPosts = (req, res) => {
-    Post.find({})
+  Post.find({})
         .populate('comments', 'text')
         .exec()
         .then((posts) => {
-            res.json(posts);
-        }) 
+          res.json(posts);
+        })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 };
 
 const findPost = (req, res) => {
-    const { id } = req.params;
-    Post.findById(id)
+  const { id } = req.params;
+  Post.findById(id)
         .populate('comments', 'text')
         .exec()
         .then((post) => {
-            res.json(post);
+          res.json(post);
         })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 };
 
 const addComment = (req, res) => {
-    const { id } = req.params;
-    const { text } = req.query;
+  const { id } = req.params;
+  const { text } = req.query;
 
-    const newComment = new Comment({ _parent: id, text });
-    newComment.save()
+  const newComment = new Comment({ _parent: id, text });
+  newComment.save()
         .then((comment) => {
-            Post.findById(id)
+          Post.findById(id)
                 .exec()
                 .then((post) => {
-                    post.comments.push(comment);
-                    post.save();
-                    res.send({ success: true });
+                  post.comments.push(comment);
+                  post.save();
+                  res.send({ success: true });
                 })
                 .catch((err) => {
-                res.status(STATUS_USER_ERROR);
-                res.json({ stack: err.stack, message: err.message });
-            });
+                  res.status(STATUS_USER_ERROR);
+                  res.json({ stack: err.stack, message: err.message });
+                });
         })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 };
 
 const deleteComment = (req, res) => {
-    const { id, commentId } = req.params;
+  const { id, commentId } = req.params;
 
-    const comment = Comment.findByIdAndRemove(commentId)
+  const comment = Comment.findByIdAndRemove(commentId)
         .exec()
         .then((comment) => {
-            return Promise.resolve(comment);
+          return Promise.resolve(comment);
         })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 
-    Post.findById(id)
+  Post.findById(id)
         .exec()
         .then((post) => {
-            const index = post.comments.findIndex((c) => {
-                c._id === comment._id;
-            });
-            post.comments.splice(index, 1);
-            res.json({ success: true });
+          const index = post.comments.findIndex((c) => {
+            c._id === comment._id;
+          });
+          post.comments.splice(index, 1);
+          res.json({ success: true });
         })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 };
-
+/* eslint no-console: 0 */
 const deletePost = (req, res) => {
-    const { id } = req.params;
-    console.log('id: ', id); 
-    const comments = Post.findByIdAndRemove(id)
+  const { id } = req.params;
+  // console.log('id: ', id);
+  const comments = Post.findByIdAndRemove(id)
         .exec()
         .then((post) => {
-            return Promise.resolve(post.comments);
+          return Promise.resolve(post.comments);
         })
         .catch((err) => {
-            res.status(STATUS_USER_ERROR);
-            res.json({ stack: err.stack, message: err.message });
+          res.status(STATUS_USER_ERROR);
+          res.json({ stack: err.stack, message: err.message });
         });
 
-    Comment.remove({
-        '_id': { $in: comments }
-    })
+  Comment.remove({
+    // '_id': { $in: comments }
+    _id: { $in: comments }
+  })
     .exec()
     .then(() => {
-        res.send({ success: true });
+      res.send({ success: true });
     })
     .catch((err) => {
-        res.status(STATUS_USER_ERROR);
-        res.json({ stack: err.stack, message: err.message });
+      res.status(STATUS_USER_ERROR);
+      res.json({ stack: err.stack, message: err.message });
     });
 };
 
 module.exports = {
-    createPost,
-    listPosts,
-    findPost,
-    addComment,
-    deleteComment,
-    deletePost
+  createPost,
+  listPosts,
+  findPost,
+  addComment,
+  deleteComment,
+  deletePost
 };
-
